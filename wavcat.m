46f17ed0b0b1d58968e476ac161b8257fb9427d9
emmushka <emcarline@gmail.com>
2018-12-10 15:45:04 -0400

    Add files via upload

---
+++ wavcat.m
@@ -0,0 +1,21 @@
+
+# Concatenate wav files into one 
+
+msgbox("Enter files to concatenate. Sample rate must be the same.")
+[file,path] = uigetfile("*.wav","Multiselect","on");
+
+if strcmp(typeinfo(file),"sq_string")
+          file_temp = file;
+          file = cell(1,1);
+          file{1,1} = file_temp;
+endif
+
+y = [];
+for i = 1:length(file)
+          [ytemp,fs] = audioread([path file{1,i}]);
+          y = [y; ytemp];
+endfor
+
+name = input("\nEnter file name:\n");
+
+audiowrite([name ".wav"], y, fs);
